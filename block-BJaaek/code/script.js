f0bda43e5e7e78c4753857d8da6ae417a8786c56
Arun <akr7205@gmail.com>
2022-06-07 22:57:01 +0530

    2nd commit

--- block-BJaaek/code/script.js
+++ block-BJaaek/code/script.js
@@ -1,14 +1,14 @@
-let banner = document.querySelector('.banner');
+let first = document.querySelector('.first');
 let second = document.querySelector('.second');
 
 
 
-banner.addEventListener('click', function getmycolor() {
+first.addEventListener('click', function getmycolor() {
     let red = Math.floor(Math.random() * 256);
     let green = Math.floor(Math.random() * 256);
     let blue = Math.floor(Math.random() * 256);
 
-    let first = document.querySelector('.first');
+    // let first = document.querySelector('.first');
 
     first.style.backgroundColor = `rgb(${red},${green},${blue})`;
 
