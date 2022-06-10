859f4c83f9cc06892305c7bee4ae2d9e80c2b4ad
Arun <akr7205@gmail.com>
2022-06-10 10:36:27 +0530

    1st coomit

---
+++ block-BJaaep/code/script.js
@@ -0,0 +1,11 @@
+let boxes1=document.querySelectorAll('.boxes1');
+
+
+// function randomNum(){
+//     return Math.floor(Math.random()*13);
+// }
+// function handelEvent(event){
+// event.target.innerHTML=randomNum();
+// }
+
+// box1.addEventListener('click', handelEvent);
