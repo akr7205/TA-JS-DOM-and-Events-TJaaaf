b69d2929b316b1237a37807e7fc1bcac50061b7e
Arun <akr7205@gmail.com>
2022-06-23 17:51:55 +0530

    1st commit

--- block-BJaaet/code/script.js
+++ block-BJaaet/code/script.js
@@ -1,13 +1,63 @@
-let form = document.querySelector('form');
-let input = document.getElementById('movie');
-let list = document.querySelector('.list');
-function handelclick(e) {
-    e.preventDefault();
-    console.dir(e.target);
-    let movieName = e.target.elements.movie;
-    console.log(movieName.value);
-    // let li = document.createElement('li');
-    // li.innerText = movieName.value;
-    // list.appendChild(li);
+let input = document.querySelector(`input[type="text"]`);
+let rootList = document.querySelector('.movie-list');
+let btn = document.querySelector('.btn');
+// console.log(input);
+let allMovies = [
+
+];
+input.addEventListener("keyup", (event) => {
+    console.dir(event);
+    console.log(event.target);
+    console.log(event.keyCode);
+    if (event.keyCode === 13) {
+        console.log(event.target.value);
+        allMovies.push({
+            name: event.target.value,
+            watched: false,
+        });
+        event.target.value='';
+        createMoviesUI();
+    }
+});
+
+function deleteMovie(event) {
+    console.log(event.target);
+    let id = event.target.dataset.id;
+    allMovies.splice(id, 1);
+    createMoviesUI();
 }
-input.addEventListener('click', handelclick);
+function handelChange(event) {
+    // console.log(event);
+    console.log(event.target);
+    console.dir(event.target);
+    let id = event.target.id;
+    allMovies[id].watched = !allMovies[id].watched;
+}
+function createMoviesUI() {
+    //     <li>
+    //     <input class="styled-checkbox" id="1" type="checkbox">
+    //     <label for="1">Checkbox</label>
+    //     <span>-</span>
+    //   </li>
+    rootList.innerHTML = '';
+    allMovies.forEach((movie, i) => {
+        let li = document.createElement('li');
+        let input = document.createElement('input');
+        input.classList.add('styled-checkbox');
+        input.id = i;
+        input.type = 'checkbox';
+        input.checked = movie.watched;
+        input.addEventListener('change', handelChange);
+        let label = document.createElement('label');
+        label.for = i;
+        label.innerText = movie.name;
+        let span = document.createElement('span');
+        span.innerText = '-';
+        span.setAttribute('data-id', i);
+        span.addEventListener('click', deleteMovie);
+        li.append(input, label, span);
+        rootList.append(li);
+    })
+};
+
+// createMoviesUI();
